8e88d7dc8bff5f0ca29be7c40dbb46d3caea2c3a
Jonas Kruckenberg <iterpre@protonmail.com>
2022-09-20 20:08:56 +0200

    fmt

--- examples/preact-app/src-tauri/src/main.rs
+++ examples/preact-app/src-tauri/src/main.rs
@@ -4,12 +4,11 @@
 )]
 
 use juniper::{
-  graphql_object, graphql_subscription, EmptyMutation, FieldResult, GraphQLObject,
-  RootNode, FieldError,
-  futures::Stream
+  futures::Stream, graphql_object, graphql_subscription, EmptyMutation, FieldError, FieldResult,
+  GraphQLObject, RootNode,
 };
-use tauri_plugin_graphql::Context as GraphQLContext;
 use std::pin::Pin;
+use tauri_plugin_graphql::Context as GraphQLContext;
 
 #[derive(GraphQLObject, Debug, Clone)]
 struct Human {
@@ -33,29 +32,18 @@ type StringStream = Pin<Box<dyn Stream<Item = Result<String, FieldError>> + Send
 
 #[graphql_subscription(context = GraphQLContext)]
 impl Subscription {
-    async fn hello_world() -> StringStream {
-        let stream = juniper::futures::stream::iter(vec![
-            Ok(String::from("Hello")),
-            Ok(String::from("World!"))
-        ]);
+  async fn hello_world() -> StringStream {
+    let stream =
+      juniper::futures::stream::iter(vec![Ok(String::from("Hello")), Ok(String::from("World!"))]);
 
-        Box::pin(stream)
-    }
+    Box::pin(stream)
+  }
 }
 
-type Schema = RootNode<
-  'static,
-  Query,
-  EmptyMutation<GraphQLContext>,
-  Subscription,
->;
+type Schema = RootNode<'static, Query, EmptyMutation<GraphQLContext>, Subscription>;
 
 fn main() {
-  let schema = Schema::new(
-    Query,
-    EmptyMutation::<GraphQLContext>::new(),
-    Subscription,
-  );
+  let schema = Schema::new(Query, EmptyMutation::<GraphQLContext>::new(), Subscription);
 
   tauri::Builder::default()
     .plugin(tauri_plugin_graphql::init(schema))
