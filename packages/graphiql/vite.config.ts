17eece0394cdcce79e78c9f636b820312a0ad0b7
Jonas Kruckenberg <iterpre@protonmail.com>
2022-09-21 15:32:44 +0200

    fmt

--- packages/graphiql/vite.config.ts
+++ packages/graphiql/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from "vite-plugin-singlefile"
+import {viteSingleFile} from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
