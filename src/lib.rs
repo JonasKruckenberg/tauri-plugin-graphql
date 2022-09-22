a331d03d33111056b597c2f399c617ebac627801
Jonas Kruckenberg <iterpre@protonmail.com>
2022-09-22 12:12:54 +0200

    split regular and graphiql plugins and make graphiql url configurable

--- src/lib.rs
+++ src/lib.rs
@@ -164,8 +164,8 @@
 //!
 //! ```rust
 //! use async_graphql::{
-//!   futures_util::{self, stream::Stream}, 
-//!   Schema, Object, Subscription, EmptySubscription, 
+//!   futures_util::{self, stream::Stream},
+//!   Schema, Object, Subscription, EmptySubscription,
 //!   EmptyMutation, SimpleObject, Result as GraphQLResult
 //! };
 //!
@@ -216,12 +216,67 @@ use async_graphql::{
   futures_util::StreamExt, BatchRequest, ObjectType, Request, Schema, SubscriptionType,
 };
 use serde::Deserialize;
+#[cfg(feature = "graphiql")]
+use std::net::SocketAddr;
 use tauri::{
   plugin::{self, TauriPlugin},
-  InvokeError, Manager, Runtime,
+  Invoke, InvokeError, Manager, Runtime,
 };
 
-/// Initializes the GraphQL plugin
+fn invoke_handler<R, Query, Mutation, Subscription>(
+  schema: Schema<Query, Mutation, Subscription>,
+) -> impl Fn(Invoke<R>)
+where
+  R: Runtime,
+  Query: ObjectType + 'static,
+  Mutation: ObjectType + 'static,
+  Subscription: SubscriptionType + 'static,
+{
+  move |invoke| {
+    let window = invoke.message.window();
+
+    let schema = schema.clone();
+
+    match invoke.message.command() {
+      "graphql" => invoke.resolver.respond_async(async move {
+        let req: BatchRequest = serde_json::from_value(invoke.message.payload().clone())
+          .map_err(InvokeError::from_serde_json)?;
+
+        let resp = schema
+          .execute_batch(req.data(window.app_handle()).data(window))
+          .await;
+
+        let str = serde_json::to_string(&resp).map_err(InvokeError::from_serde_json)?;
+
+        Ok((str, resp.is_ok()))
+      }),
+      "subscriptions" => invoke.resolver.respond_async(async move {
+        let req: SubscriptionRequest = serde_json::from_value(invoke.message.payload().clone())
+          .map_err(InvokeError::from_serde_json)?;
+
+        let subscription_window = window.clone();
+        let mut stream = schema.execute_stream(req.inner.data(window.app_handle()).data(window));
+
+        let event_id = &format!("graphql://{}", req.id);
+
+        while let Some(result) = stream.next().await {
+          let str = serde_json::to_string(&result).map_err(InvokeError::from_serde_json)?;
+
+          subscription_window.emit(event_id, str)?;
+        }
+        subscription_window.emit(event_id, Option::<()>::None)?;
+
+        Ok(())
+      }),
+      cmd => invoke.resolver.reject(format!(
+        "Invalid endpoint \"{}\". Valid endpoints are: \"graphql\", \"subscriptions\".",
+        cmd
+      )),
+    }
+  }
+}
+
+/// Initializes the GraphQL plugin.
 ///
 /// This plugin exposes a async-graphql endpoint via Tauri's IPC system,
 /// allowing the frontend to invoke backend functionality through GraphQL.
@@ -271,102 +326,115 @@ where
   Mutation: ObjectType + 'static,
   Subscription: SubscriptionType + 'static,
 {
-  #[cfg(feature = "graphiql")]
-  let schema2 = schema.clone();
+  plugin::Builder::new("graphql")
+    .invoke_handler(invoke_handler(schema))
+    .build()
+}
+
+/// Initializes the GraphQL plugin and GraphiQL IDE.
+///
+/// This plugin exposes a async-graphql endpoint via Tauri's IPC system,
+/// allowing the frontend to invoke backend functionality through GraphQL.
+/// While the regular `init` function does not open a web server, the GraphiQL
+/// client is exposed over http.
+///
+/// The `schema` argument must be a valid [`async_graphql::Schema`].
+///
+/// ## Example
+///
+/// ```rust
+/// use async_graphql::{Schema, Object, EmptyMutation, EmptySubscription, SimpleObject, Result as GraphQLResult};
+///
+/// #[derive(SimpleObject)]
+/// struct User {
+///     id: i32,
+///     name: String
+/// }
+///
+/// struct Query;
+///
+/// // Implement resolvers for all possible queries.
+/// #[Object]
+/// impl Query {
+///     async fn me(&self) -> GraphQLResult<User> {
+///         Ok(User {
+///             id: 1,
+///             name: "Luke Skywalker".to_string(),
+///         })
+///     }
+/// }
+///
+/// let schema = Schema::new(
+///     Query,
+///     EmptyMutation,
+///     EmptySubscription,
+/// );
+///
+/// tauri::Builder::default()
+///     .plugin(tauri_plugin_graphql::init_with_graphiql(schema, ([127,0,0,1], 8080)));
+/// ```
+#[cfg(feature = "graphiql")]
+pub fn init_with_graphiql<R, Query, Mutation, Subscription>(
+  schema: Schema<Query, Mutation, Subscription>,
+  graphiql_addr: impl Into<SocketAddr>,
+) -> TauriPlugin<R>
+where
+  R: Runtime,
+  Query: ObjectType + 'static,
+  Mutation: ObjectType + 'static,
+  Subscription: SubscriptionType + 'static,
+{
+  use async_graphql::http::GraphiQLSource;
+  use async_graphql_warp::{GraphQLBadRequest, GraphQLResponse};
+  use http::StatusCode;
+  use std::convert::Infallible;
+  use warp::{http::Response as HttpResponse, Filter, Rejection};
+
+  let graphiql_addr: SocketAddr = graphiql_addr.into();
 
   plugin::Builder::new("graphql")
+    .invoke_handler(invoke_handler(schema.clone()))
     .setup(move |_| {
-      #[cfg(feature = "graphiql")]
-      {
-        use async_graphql::http::GraphiQLSource;
-        use async_graphql_warp::{GraphQLBadRequest, GraphQLResponse};
-        use http::StatusCode;
-        use std::convert::Infallible;
-        use warp::{http::Response as HttpResponse, Filter, Rejection};
+      let graphql_post = async_graphql_warp::graphql(schema).and_then(
+        |(schema, request): (
+          Schema<Query, Mutation, Subscription>,
+          async_graphql::Request,
+        )| async move {
+          Ok::<_, Infallible>(GraphQLResponse::from(schema.execute(request).await))
+        },
+      );
 
-        let graphql_post = async_graphql_warp::graphql(schema2).and_then(
-          |(schema, request): (
-            Schema<Query, Mutation, Subscription>,
-            async_graphql::Request,
-          )| async move {
-            Ok::<_, Infallible>(GraphQLResponse::from(schema.execute(request).await))
-          },
-        );
+      let graphiql = warp::path::end().and(warp::get()).map(move || {
+        HttpResponse::builder()
+          .header("content-type", "text/html")
+          .body(
+            GraphiQLSource::build()
+              .endpoint(&graphiql_addr.to_string())
+              .finish(),
+          )
+      });
 
-        let graphiql = warp::path::end().and(warp::get()).map(|| {
-          HttpResponse::builder()
-            .header("content-type", "text/html")
-            .body(
-              GraphiQLSource::build()
-                .endpoint("http://localhost:8000")
-                .finish(),
-            )
+      let routes = graphiql
+        .or(graphql_post)
+        .recover(|err: Rejection| async move {
+          if let Some(GraphQLBadRequest(err)) = err.find() {
+            return Ok::<_, Infallible>(warp::reply::with_status(
+              err.to_string(),
+              StatusCode::BAD_REQUEST,
+            ));
+          }
+
+          Ok(warp::reply::with_status(
+            "INTERNAL_SERVER_ERROR".to_string(),
+            StatusCode::INTERNAL_SERVER_ERROR,
+          ))
         });
 
-        let routes = graphiql
-          .or(graphql_post)
-          .recover(|err: Rejection| async move {
-            if let Some(GraphQLBadRequest(err)) = err.find() {
-              return Ok::<_, Infallible>(warp::reply::with_status(
-                err.to_string(),
-                StatusCode::BAD_REQUEST,
-              ));
-            }
+      println!("GraphiQL IDE: {}", graphiql_addr);
 
-            Ok(warp::reply::with_status(
-              "INTERNAL_SERVER_ERROR".to_string(),
-              StatusCode::INTERNAL_SERVER_ERROR,
-            ))
-          });
-
-        println!("GraphiQL IDE: http://localhost:8000");
-
-        tauri::async_runtime::spawn(warp::serve(routes).run(([0, 0, 0, 0], 8000)));
-      }
+      tauri::async_runtime::spawn(warp::serve(routes).run(graphiql_addr));
 
       Ok(())
-    })
-    .invoke_handler(move |invoke| {
-      let window = invoke.message.window();
-
-      let schema = schema.clone();
-
-      match invoke.message.command() {
-        "graphql" => invoke.resolver.respond_async(async move {
-          let req: BatchRequest = serde_json::from_value(invoke.message.payload().clone())
-            .map_err(InvokeError::from_serde_json)?;
-
-          let resp = schema
-            .execute_batch(req.data(window.app_handle()).data(window))
-            .await;
-
-          let str = serde_json::to_string(&resp).map_err(InvokeError::from_serde_json)?;
-
-          Ok((str, resp.is_ok()))
-        }),
-        "subscriptions" => invoke.resolver.respond_async(async move {
-          let req: SubscriptionRequest = serde_json::from_value(invoke.message.payload().clone())
-            .map_err(InvokeError::from_serde_json)?;
-
-          let subscription_window = window.clone();
-          let mut stream = schema.execute_stream(req.inner.data(window.app_handle()).data(window));
-
-          let event_id = &format!("graphql://{}", req.id);
-
-          while let Some(result) = stream.next().await {
-            let str = serde_json::to_string(&result).map_err(InvokeError::from_serde_json)?;
-
-            subscription_window.emit(event_id, str)?;
-          }
-          subscription_window.emit(event_id, Option::<()>::None)?;
-
-          Ok(())
-        }),
-        cmd => invoke.resolver.reject(format!(
-          "Invalid endpoint \"{}\". Valid endpoints are: \"graphql\", \"subscriptions\".",
-          cmd
-        )),
-      }
     })
     .build()
 }
